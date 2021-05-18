1369426ac5ec59bfaf82bb243fa94ba94f665875
Martin Luaces <martinluaces17@gmail.com>
2021-05-18 20:08:50 +0000

    tlcf

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,101 @@
-import React, { useState } from "react";
-import Lista from "./Lista.js";
+import React, { useState, useEffect } from "react";
 
 export function Home() {
+	const [todoList, setTodoList] = useState([]);
+	const [mouse, setMouse] = useState();
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const handleKeyPress = e => {
+		if (e.target.value !== "" && e.charCode === 13) {
+			let newTarea = {
+				label: e.target.value,
+				done: false
+			};
+			let newTareaList = [...todoList, newTarea];
+			setTodoList(newTareaList);
+			updateData(newTareaList);
+			e.target.value = "";
+		}
+	};
+
+	const eliminar = i => {
+		let nuevaLista = todoList.filter((elem, index) => {
+			if (index != i) {
+				return elem;
+			}
+		});
+		setTodoList(nuevaLista);
+		updateData(nuevaLista);
+	};
+
+	const mouseencima = i => {
+		setMouse(i);
+	};
+
+	const getData = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/martinluaces17")
+			.then(resp => resp.json())
+			.then(data => setTodoList(data))
+			.catch(error => console.log(error));
+	};
+
+	const updateData = updatedList => {
+		let updatedListSend = JSON.stringify(updatedList);
+		let options = {
+			method: "PUT",
+			body: updatedListSend,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/martinluaces17",
+			options
+		)
+			.then(resp => resp.json())
+			.then(data => console.log(data))
+			.catch(error => console.log(error));
+	};
 	return (
-		<div className="text-center mt-5">
-			<Lista />
+		<div className="container">
+			<div className="row d-flex justify-content-center container">
+				<div className="text-center mt-3">Agregar Tarea.</div>
+				<div className="card col-12 mt-3 p-0 mb-3">
+					<input
+						onKeyPress={handleKeyPress}
+						placeholder="Agregar tarea"
+						className="form-control p-1"
+					/>
+					<ul className="list-group list-group-flush">
+						{todoList.length === 0 ? (
+							<li className="list-group-item p-1">Cargando...</li>
+						) : (
+							todoList.map((tarea, i) => (
+								<li
+									key={i}
+									className="list-group-item p-1"
+									onClick={() => {
+										eliminar(i);
+									}}
+									onMouseOver={() => {
+										mouseencima(i);
+									}}>
+									{tarea.label}
+									<i
+										type="button"
+										className={
+											"fas fa-trash-alt float-right" +
+											(mouse == i ? "" : " hide")
+										}></i>
+								</li>
+							))
+						)}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
